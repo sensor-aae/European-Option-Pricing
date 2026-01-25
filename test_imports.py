7f731563afcde3e1e1856e4d068d241798f613e0
Amanda Achiangia <amanda.achiangia@gmail.com>
2026-01-25 13:29:54 -0800

    Convert test_imports.py into pytest test function
    Added a test function to verify imports of core packages.

--- test_imports.py
+++ test_imports.py
@@ -1,5 +1,7 @@
 # Test whether all dependencies are installed correctly
-import numpy
-import scipy
-import matplotlib.pyplot as plt
-print("✅ All core packages imported successfully.")
+
+def test_dependencies_import():
+    import numpy
+    import scipy
+    import matplotlib.pyplot as plt
+    import streamlit
